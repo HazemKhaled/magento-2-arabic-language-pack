1cb682f688a6e2b9db7db21d6b71793933143792
Anouar Kacem <anouar.k.tn@ieee.org>
2018-04-27 18:03:49 +0300

    updated catalog service : * Get Product by SKU, * Get Products by Page, * Get Categories (by page)

--- services/catalog.service.js
+++ services/catalog.service.js
@@ -30,32 +30,55 @@ module.exports = {
 		/**
 		 * Get product by SKU
 		 *
-		 * @returns
+		 * @returns {Object} Product
 		 */
-		product(ctx) {
-			if (Object.keys(ctx.params).length > 0) {
-				console.log("Putin de ctx", ctx.params);
-				return `Welcome, ${ctx.params.name}`;
-			} else {
-				return {
-					"errorCode": 404,
-					"errorMessage": "SKU(s) out of stock.",
-					"data": {}
-				};
-			}
+		product: {
+			auth: "required",
+			async handler(ctx) {
+				if (Object.keys(ctx.params).length > 0 && ctx.params.hasOwnProperty("sku")) {
+					const es = require("../libs/elastic");
+					const esClient = new es();
+					const product = await esClient.fetchProduct("products", "Product", ctx.params.sku);
+					return product;
+				} else {
+					return {
+						"errorCode": 404,
+						"errorMessage": "SKU(s) out of stock.",
+						"data": {}
+					};
+				}
+			}			
 		},
 
 		/**
-		 * Welcome a username
+		 * Get Products By Page
 		 *
-		 * @param {String} name - User name
+		 * @returns {Array} 10 - 1000 products per page
 		 */
 		products: {
 			auth: "required",
-			handler() {
-				return "Welcome, idiot";
+			async handler(ctx) {
+				const es = require("../libs/elastic");
+				const esClient = new es();
+				const products = await esClient.fetch("products", "Product", ctx.params.hasOwnProperty("page") ? ctx.params.page : undefined);
+				return products;				
 			}			
 		},
+
+		/**
+		 * Get Products By Page
+		 *
+		 * @returns {Array} 10 - 1000  categories per page
+		 */
+		categories: {
+			auth: "required",
+			async handler(ctx) {
+				const es = require("../libs/elastic");
+				const esClient = new es();
+				const categories = await esClient.fetch("categories", "Category", ctx.params.hasOwnProperty("page") ? ctx.params.page : undefined);
+				return categories;
+			}
+		}
 	},
 
 	/**
