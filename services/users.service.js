3ad106b7bdab94da3b7e884ff73e36c4ef3984d6
Anouar Kacem <anouar.k.tn@ieee.org>
2018-05-07 15:11:18 +0300

    users api clean up fixed & custimized authentication via klayer

--- services/users.service.js
+++ services/users.service.js
@@ -39,53 +39,6 @@ module.exports = {
 	 */
 	actions: {
 		/**
-		 * Register a new user
-		 * 
-		 * @actions
-		 * @param {Object} user - User entity
-		 * 
-		 * @returns {Object} Created entity & token
-		 */
-		create: {
-			params: {
-				user: { type: "object" }
-			},			
-			handler(ctx) {
-				let entity = ctx.params.user;
-				return this.validateEntity(entity)
-					.then(() => {
-						if (entity.username)
-							return this.adapter.findOne({ username: entity.username })
-								.then(found => {
-									if (found)
-										return Promise.reject(new MoleculerClientError("Username is exist!", 422, "", [{ field: "username", message: "is exist"}]));
-									
-								});
-					})
-					.then(() => {
-						if (entity.email)
-							return this.adapter.findOne({ email: entity.email })
-								.then(found => {
-									if (found)
-										return Promise.reject(new MoleculerClientError("Email is exist!", 422, "", [{ field: "email", message: "is exist"}]));
-								});
-							
-					})
-					.then(() => {
-						entity.password = bcrypt.hashSync(entity.password, 10);
-						entity.bio = entity.bio || "";
-						entity.image = entity.image || null;
-						entity.createdAt = new Date();
-
-						return this.adapter.insert(entity)
-							.then(doc => this.transformDocuments(ctx, {}, doc))
-							.then(user => this.transformEntity(user, true, ctx.meta.token))
-							.then(json => this.entityChanged("created", json, ctx).then(() => json));					
-					});
-			}
-		},
-
-		/**
 		 * Login with username & password
 		 * 
 		 * @actions
@@ -101,25 +54,28 @@ module.exports = {
 				}}
 			},
 			handler(ctx) {
-				const { consumer_key, consumer_secret } = ctx.params;
+				const { consumer_key, consumer_secret } = ctx.params.user;
 
 				return this.Promise.resolve()
 					.then(async () => {
 						const KlayerLib = require("../libs/klayer");
 						const klayer = new KlayerLib();
-
 						try {
 							let instance = await klayer.findInstance(consumer_key);
 							instance = instance[0];
 							if (!instance) {
 								return this.Promise.reject(new MoleculerClientError("consumer_key or consumer_secret is invalid!", 422, "", [{ field: "consumer_key", message: "is not found"}]));
 							} else {
-								return {
-									"_id": instance.webhook_hash,
-									"url": instance.url,
-									"status": instance.status,
-									"base_currency": instance.base_currency,
-								};
+								if (consumer_key === instance.webhook_hash && consumer_secret === instance.secret) {
+									return {
+										"_id": instance.webhook_hash,
+										"url": instance.url,
+										"status": instance.status,
+										"base_currency": instance.base_currency,
+									};
+								} else {
+									return this.Promise.reject(new MoleculerClientError("consumer_key or consumer_secret is invalid!", 422, "", [{ field: "consumer_key", message: "is not valid"}, { field: "consumer_secret", message: "is not valid"}]));
+								}								
 							}
 						} catch(err) {
 							return this.Promise.reject(new MoleculerClientError(err));
@@ -128,26 +84,6 @@ module.exports = {
 					.then(user => this.transformEntity(user, true, ctx.meta.token));
 			}
 		},
-		/*login: {
-			params: {
-				user: { type: "object", props: {
-					consumer_key: { type: "string" },
-					consumer_secret: { type: "string", min: 1 }
-				}}
-			},
-			handler(ctx) {
-				const { consumer_key, consumer_secret } = ctx.params.user;
-				console.log({ consumer_key, consumer_secret });
-				return this.Promise.resolve()
-					.then(() => {
-						return {
-							email: "ok@",
-							password: consumer_secret
-						};
-					})
-					.then(user => this.transformEntity(user, true, ctx.meta.token));
-			}
-		},*/
 
 		/**
 		 * Get user by JWT token (for API GW authentication)
@@ -181,7 +117,7 @@ module.exports = {
 						if (decoded.id) {
 							const instance = await klayer.findInstance(decoded.id);
 							if (instance[0].status === "confirmed") {
-								return true;
+								return decoded;
 							}
 						}
 					});
@@ -218,46 +154,16 @@ module.exports = {
 		 */
 		transformEntity(user, withToken, token) {
 			if (user) {
-				//user.image = user.image || "https://www.gravatar.com/avatar/" + crypto.createHash("md5").update(user.email).digest("hex") + "?d=robohash";
-				user.image = user.image || "";
 				if (withToken)
 					user.token = token || this.generateJWT(user);
 			}
 
 			return { user };
-		},
-
-		/**
-		 * Transform returned user entity as profile.
-		 * 
-		 * @param {Context} ctx
-		 * @param {Object} user 
-		 * @param {Object?} loggedInUser 
-		 */
-		transformProfile(ctx, user, loggedInUser) {
-			//user.image = user.image || "https://www.gravatar.com/avatar/" + crypto.createHash("md5").update(user.email).digest("hex") + "?d=robohash";
-			user.image = user.image || "https://static.productionready.io/images/smiley-cyrus.jpg";
-
-			if (loggedInUser) {
-				return ctx.call("follows.has", { user: loggedInUser._id.toString(), follow: user._id.toString() })
-					.then(res => {
-						user.following = res;
-						return { profile: user };
-					});
-			}
-
-			user.following = false;
-
-			return { profile: user };
 		}
 	},
 
 	events: {
 		"cache.clean.users"() {
-			if (this.broker.cacher)
-				this.broker.cacher.clean(`${this.name}.*`);
-		},
-		"cache.clean.follows"() {
 			if (this.broker.cacher)
 				this.broker.cacher.clean(`${this.name}.*`);
 		}
