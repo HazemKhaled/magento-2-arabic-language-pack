95a58d10e8825e97b7b42c44d902fa83afeab3c7
Anouar Kacem <anouar.k.tn@ieee.org>
2018-05-16 16:23:48 +0300

    test user login

--- test/unit/users.spec.js
+++ test/unit/users.spec.js
@@ -2,16 +2,16 @@ const { ServiceBroker } = require('moleculer');
 const { ValidationError } = require('moleculer').Errors;
 const TestService = require('../../services/users.service');
 
-describe("Test 'greeter' service", () => {
+describe("Test 'users.login' service", () => {
   const broker = new ServiceBroker();
   broker.createService(TestService);
 
   beforeAll(() => broker.start());
   afterAll(() => broker.stop());
 
-  describe("Test 'auth.login' action", () => {
+  describe("Test 'users.login' action", () => {
     it('should reject an ValidationError', () => {
-      expect(broker.call('auth.login')).rejects.toBeInstanceOf(ValidationError);
+      expect(broker.call('users.login')).rejects.toBeInstanceOf(ValidationError);
     });
   });
 });
