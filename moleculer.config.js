98ae1017f9cba8f560d6c83c138a55e73419b3e4
Hazem Khaled <hazem@knawat.com>
2018-06-08 13:18:17 +0300

    clean up config file

--- moleculer.config.js
+++ moleculer.config.js
@@ -1,44 +1,9 @@
 module.exports = {
-  namespace: '',
-  nodeID: null,
-
   logger: true,
   logLevel: 'info',
-  logFormatter: 'default',
-
-  serializer: 'JSON',
-
   cacher: null,
+  metrics: true,
 
-  requestTimeout: 10 * 1000,
-  requestRetry: 0,
-  maxCallLevel: 100,
-  heartbeatInterval: 5,
-  heartbeatTimeout: 15,
-
-  disableBalancer: false,
-
-  registry: {
-    strategy: 'RoundRobin',
-    preferLocal: true
-  },
-
-  circuitBreaker: {
-    enabled: false,
-    maxFailures: 3,
-    halfOpenTime: 10 * 1000,
-    failureOnTimeout: true,
-    failureOnReject: true
-  },
-
-  validation: true,
-  validator: null,
-  metrics: false,
-  metricsRate: 1,
-  statistics: false,
-  internalActions: true,
-
-  hotReload: true,
-
-  replCommands: null
+  statistics: true,
+  hotReload: true
 };
