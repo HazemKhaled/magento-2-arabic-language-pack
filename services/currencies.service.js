a0c8c46349c96d53fd956f37ceecd76a867213f4
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-05-08 11:49:57 +0300

    Add: get Currencies APIs

---
+++ services/currencies.service.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+  name: 'currencies',
+  settings: {
+    AUTH: Buffer.from(`${process.env.BASIC_USER}:${process.env.BASIC_PASS}`).toString('base64')
+  },
+  actions: {
+    getCurrency: {
+      params: {
+        currency: { type: 'string', min: 3, max: 3 }
+      },
+      handler(ctx) {
+        return fetch(`${process.env.OMS_URL}/currencies/${ctx.params.currency}`, {
+          method: 'get',
+          headers: {
+            Authorization: `Basic ${this.settings.AUTH}`
+          }
+        }).then(res => res.json());
+      }
+    },
+    getCurrencies: {
+      handler() {
+        return fetch(`${process.env.OMS_URL}/currencies`, {
+          method: 'get',
+          headers: {
+            Authorization: `Basic ${this.settings.AUTH}`
+          }
+        }).then(res => res.json());
+      }
+    }
+  }
+};
