cf6f773bf3f27df88d4ba32b64c6dca268194316
Hazem Khaled <hazem.khaled@gmail.com>
2019-05-12 20:15:20 +0300

    port currency to ts

--- services/currencies.service.ts
+++ services/currencies.service.ts
@@ -1,6 +1,7 @@
-const fetch = require('node-fetch');
+import { ServiceSchema } from 'moleculer';
+import fetch from 'node-fetch';
 
-module.exports = {
+export const CurrenciesService: ServiceSchema = {
   name: 'currencies',
   settings: {
     AUTH: Buffer.from(`${process.env.BASIC_USER}:${process.env.BASIC_PASS}`).toString('base64')
@@ -14,13 +15,14 @@ module.exports = {
         key: ['currencyCode'],
         ttl: 30 * 60 // 1 hour
       },
-      handler(ctx) {
+      handler(ctx: any) {
         return fetch(`${process.env.OMS_URL}/currencies/${ctx.params.currency}`, {
           method: 'get',
           headers: {
             Authorization: `Basic ${this.settings.AUTH}`
           }
         }).then(res => res.json());
+        // TODO: transform currency before response
       }
     },
     getCurrencies: {
