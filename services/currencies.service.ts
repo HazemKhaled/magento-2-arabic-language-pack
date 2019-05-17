c4bd726303853f28d15b0739695304a357937272
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-05-17 10:52:58 +0300

    Change: Currencies API to openexchange

--- services/currencies.service.ts
+++ services/currencies.service.ts
@@ -1,13 +1,16 @@
+import { Currency } from './../types/currency.d';
 import { ServiceSchema } from 'moleculer';
+import * as Cron from 'moleculer-cron';
+import * as DbService from 'moleculer-db';
 import fetch from 'node-fetch';
 
 const TheService: ServiceSchema = {
   name: 'currencies',
-  settings: {
-    AUTH: Buffer.from(`${process.env.BASIC_USER}:${process.env.BASIC_PASS}`).toString('base64')
-  },
+  mixins: [Cron, DbService],
+  adapter: new DbService.MemoryAdapter({ filename: 'db/currency.db' }),
   actions: {
     getCurrency: {
+      auth: 'Basic',
       params: {
         currencyCode: { type: 'string', min: 3, max: 3 }
       },
@@ -16,26 +19,62 @@ const TheService: ServiceSchema = {
         ttl: 30 * 60 // 1 hour
       },
       handler(ctx: any) {
-        return fetch(`${process.env.OMS_URL}/currencies/${ctx.params.currency}`, {
-          method: 'get',
-          headers: {
-            Authorization: `Basic ${this.settings.AUTH}`
-          }
-        }).then(res => res.json());
-        // TODO: transform currency before response
+        return ctx
+          .call('currencies.find', {
+            query: { _id: ctx.params.currencyCode.toUpperCase() }
+          })
+          .then(async (currency: Currency[]) => {
+            if (currency.length === 0) {
+              ctx.meta.statusCode = 404;
+              return { warning: 'Currency code could not be found!' };
+            }
+            if (new Date(currency[0].lastUpdate).getTime() - Date.now() > 3600 * 1000) {
+              currency = await ctx.call('currencies.getCurrencies').then(() =>
+                ctx.call('currencies.find', {
+                  query: { _id: ctx.params.currencyCode.toUpperCase() }
+                })
+              );
+            }
+            delete currency[0].lastUpdate;
+            currency[0].currencyCode = currency[0]._id;
+            delete currency[0]._id;
+            return currency;
+          });
       }
     },
     getCurrencies: {
+      auth: 'Basic',
       cache: {
         ttl: 30 * 60 // 1 hour
       },
-      handler() {
-        return fetch(`${process.env.OMS_URL}/currencies`, {
-          method: 'get',
-          headers: {
-            Authorization: `Basic ${this.settings.AUTH}`
+      handler(ctx) {
+        return ctx.call('currencies.find').then((currencies: Currency[]) => {
+          // If rates are more than 1 hour
+          if (new Date(currencies[0].lastUpdate).getTime() - Date.now() > 3600 * 1000) {
+            // Get new rates
+            return fetch(`https://openexchangerates.org/api/latest.json`, {
+              method: 'get',
+              headers: { Authorization: `Token ${process.env.OPENEXCHANGE_TOKEN}` }
+            })
+              .then(res => res.json())
+              .then(async newCurrencies => {
+                const currenciesObj = Object.keys(newCurrencies.rates).map(currency => ({
+                  _id: currency,
+                  rate: newCurrencies.rates[currency],
+                  lastUpdate: new Date()
+                }));
+
+                // Push the new rates into the DB
+                currencies = await ctx.call('currencies.insert', { entities: currenciesObj });
+              });
           }
-        }).then(res => res.json());
+
+          // Return the currencies rates
+          return currencies.map((currency: Currency) => ({
+            currencyCode: currency._id,
+            rate: currency.rate
+          }));
+        });
       }
     }
   }
---
+++ types/currency.d.ts
@@ -0,0 +1,6 @@
+export interface Currency {
+  _id?: string;
+  currencyCode?: string;
+  rate: number;
+  lastUpdate?: Date;
+}
