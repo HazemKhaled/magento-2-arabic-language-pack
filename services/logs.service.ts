143fc9e19527733ae9b83fa9000972eb16fba516
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-07-17 08:44:23 +0300

    fix: log topicId to accept numeric ids

--- services/logs.service.ts
+++ services/logs.service.ts
@@ -24,7 +24,10 @@ const TheService: ServiceSchema = {
         message: { type: 'string' },
         logLevel: { type: 'enum', values: ['info', 'debug', 'error', 'warn'] },
         storeId: { type: 'string', optional: true },
-        topicId: { type: 'string', optional: true },
+        topicId: [
+          { type: 'string', optional: true },
+          { type: 'number', optional: true, convert: true, integer: true }
+        ],
         payload: { type: 'object', optional: true },
         code: { type: 'number', convert: true, integer: true }
         // Remove until it's added to index.d
@@ -89,7 +92,10 @@ const TheService: ServiceSchema = {
         topic: { type: 'string', optional: true },
         sort: { type: 'enum', values: ['asc', 'desc'], optional: true },
         logLevel: { type: 'enum', values: ['info', 'debug', 'error', 'warn'], optional: true },
-        storeId: { type: 'string', optional: true },
+        storeId: [
+          { type: 'string', optional: true },
+          { type: 'number', optional: true, convert: true, integer: true }
+        ],
         topicId: { type: 'string', optional: true },
         limit: { type: 'number', optional: true, min: 1, max: 500, convert: true },
         page: { type: 'number', optional: true, min: 1, convert: true }
