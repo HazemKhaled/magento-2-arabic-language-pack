4964695497d57c9419c0115af182a8771ba68103
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-07-18 09:37:19 +0300

    clean: order type

--- utilities/types/order.type.ts
+++ utilities/types/order.type.ts
@@ -7,22 +7,20 @@
 export interface Order {
   id?: string;
   status: string;
-  state?: string;
   knawat_order_status?: string;
   line_items?: OrderItem[];
   items?: OrderItem[];
   billing?: object;
   shipping: object;
   total?: number;
-  date_created?: Date;
   createDate?: Date;
-  knawat_status?: Date;
-  meta_data?: {
-    length: number;
-    forEach: (arg0: (meta: any) => void) => void;
-  };
-
-  [key: string]: any;
+  externalId?: string;
+  updateDate?: Date;
+  notes?: string;
+  externalInvoice?: string;
+  invoice_url?: string;
+  shipmentCourier?: string;
+  shipping_method?: string;
 }
 
 /**
