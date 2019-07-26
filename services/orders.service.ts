62846eea151f4e7fa5cdae0f859c76fa1d9dbe92
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-07-26 18:20:46 +0300

    fix: PROCESSING-FEES

--- services/orders.service.ts
+++ services/orders.service.ts
@@ -153,8 +153,7 @@ const TheService: ServiceSchema = {
           (subscription.attr_order_processing_fees && subscription.attr_order_processing_fees > 0)
         ) {
           data.adjustment = Number(subscription.attr_order_processing_fees || 2);
-          data.adjustmentDescription = `PROCESSING-FEES: ${subscription.attr_order_processing_fees ||
-            2}`;
+          data.adjustmentDescription = `PROCESSING-FEES: `;
         }
         // Preparing billing data
         data.billing = this.checkAddress(instance, data.externalId) ? instance.address : undefined;
