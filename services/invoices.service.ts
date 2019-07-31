df862c5b949b30f6852b8d2af113501375cff9af
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-07-31 15:00:30 +0300

    add: invoices cache & query params

--- services/invoices.service.ts
+++ services/invoices.service.ts
@@ -9,12 +9,29 @@ const TheService: ServiceSchema = {
   actions: {
     get: {
       auth: 'Bearer',
+      cache: { keys: ['#user'], ttl: 60 },
+      params: {
+        page: { type: 'number', integer: true, optional: true, convert: true },
+        limit: { type: 'number', integer: true, optional: true, convert: true },
+        reference_number: { type: 'string', optional: true },
+        invoice_number: { type: 'string', optional: true }
+      },
       async handler(ctx: Context) {
         const instance = await ctx.call('stores.findInstance', {
           consumerKey: ctx.meta.user
         });
+        const url = new URL(`${process.env.OMS_BASEURL}/invoices/${instance.internal_data.omsId}`);
+        const keys: { [key: string]: string } = {
+          page: 'page',
+          limit: 'perPage',
+          reference_number: 'reference_number',
+          invoice_number: 'invoice_number'
+        };
+        Object.keys(ctx.params).forEach(key => {
+          if (ctx.params[key]) url.searchParams.append(keys[key], ctx.params[key]);
+        });
         if (instance.internal_data && instance.internal_data.omsId) {
-          return fetch(`${process.env.OMS_BASEURL}/invoices/${instance.internal_data.omsId}`, {
+          return fetch(url.href, {
             method: 'get',
             headers: {
               Authorization: `Basic ${this.settings.AUTH}`
