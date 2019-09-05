25f486d5a60c1f2b846223d2441e989c408e1574
Ahmed ElGreetly <aelgreetly@yahoo.com>
2019-09-05 17:56:02 +0300

    update: categories cache to 10min for test

--- services/categories.service.ts
+++ services/categories.service.ts
@@ -38,7 +38,7 @@ const TheService: ServiceSchema = {
     list: {
       auth: 'Bearer',
       cache: {
-        ttl: 60 * 60 // 1 hour
+        ttl: 60 * 10 // 1 hour
       },
       params: {
         parentId: [{ type: 'number', optional: true }, { type: 'string', optional: true }],
