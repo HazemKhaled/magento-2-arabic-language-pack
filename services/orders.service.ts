15f73a4792ea5dba0539bd976650f8425e86ca93
Ahmed Elgreetly <ahmedelgreetly@ahmeds-air.mynet>
2019-09-30 11:39:42 +0300

    fix: order details message

--- services/orders.service.ts
+++ services/orders.service.ts
@@ -190,7 +190,7 @@ const TheService: ServiceSchema = {
             storeId: instance.url,
             logLevel: 'error',
             code: result.error.statusCode,
-            payload: { errors: result, params: ctx.params }
+            payload: { errors: (result.error && result.error.details) || result, params: ctx.params }
           });
           ctx.meta.$statusCode = result.error.statusCode;
           ctx.meta.$statusMessage = result.error.name;
@@ -414,7 +414,7 @@ const TheService: ServiceSchema = {
               storeId: instance.url,
               logLevel: 'error',
               code: result.error.statusCode,
-              payload: { errors: result, params: ctx.params }
+              payload: { errors: (result.error && result.error.details) || result, params: ctx.params }
             });
             ctx.meta.$statusCode = result.error.statusCode;
             ctx.meta.$statusMessage = result.error.name;
