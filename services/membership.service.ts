a5a2f772a5a918ac0b3f31ad4073aab1668f04bd
Ahmed ElGreetly <elgreetly@knawat.com>
2019-11-14 14:53:09 +0200

    add: membership sub endpoints

--- services/membership.service.ts
+++ services/membership.service.ts
@@ -1,105 +1,126 @@
 import { Context, Errors, ServiceSchema } from 'moleculer';
 import DbService from '../utilities/mixins/mongo.mixin';
 import { Membership } from '../utilities/types';
-import { CreateMembershipValidation } from '../utilities/validations';
+import { CreateMembershipValidation, UpdateMembershipValidation } from '../utilities/validations';
 const MoleculerError = Errors.MoleculerError;
 
 const TheService: ServiceSchema = {
-    name: 'membership',
-    mixins: [DbService('membership')],
-    actions: {
-        create: {
-            auth: 'Basic',
-            params: CreateMembershipValidation,
-            async handler(ctx: Context): Promise<Membership> {
-                const {params} = ctx;
-                params._id = `m-${params.id || Date.now()}`;
-                delete params.id;
-                if(params.isDefault) {
-                    const currentDefault = await this.adapter.findOne({isDefault: true, active: true});
-                    if(currentDefault) {
-                        throw new MoleculerError('There is an active default you add new one');
-                    }
-                }
-                return this.adapter
-                    .insert(params)
-                    .then((res: Membership) => {
-                        this.broker.cacher.clean(`membership.list:**`);
-                        return this.normalizeId(res)
-                    })
-                    .catch((err: any) => {
-                        if (err.name === 'MoleculerError') {
-                            throw new MoleculerError(err.message, err.code);
-                        }
-                        throw new MoleculerError(err, 500);
-                    });
+  name: 'membership',
+  mixins: [DbService('membership')],
+  actions: {
+    create: {
+      auth: 'Basic',
+      params: CreateMembershipValidation,
+      async handler(ctx: Context): Promise<Membership> {
+        const { params } = ctx;
+        params._id = `m-${params.id || Date.now()}`;
+        delete params.id;
+        if (params.isDefault) {
+          const currentDefault = await this.adapter.findOne({ isDefault: true, active: true });
+          if (currentDefault) {
+            throw new MoleculerError('There is an active default you add new one');
+          }
+        }
+        return this.adapter
+          .insert(params)
+          .then((res: Membership) => {
+            this.broker.cacher.clean(`membership.list:**`);
+            return this.normalizeId(res);
+          })
+          .catch((err: any) => {
+            if (err.name === 'MoleculerError') {
+              throw new MoleculerError(err.message, err.code);
             }
-        },
-        get: {
-            auth: 'Basic',
-            params: {
-                id: [{ type: 'string' }, { type: 'number' }]
-            },
-            cache: {
-              keys: ['id'],
-              ttl: 60 * 60 // 1 hour
-            },
-            handler(ctx: Context): Promise<Membership> {
-                return this.adapter
-                    .findOne({_id: ctx.params.id, active: true})
-                    .then((res: Membership) => {
-                        if (!res) {
-                            return this.adapter
-                            .findOne({isDefault: true}).then((def: Membership) => this.normalizeId(def))
-                        }
-                        return this.normalizeId(res);
-                    })
-                    .catch((err: any) => {
-                        if (err.name === 'MoleculerError') {
-                            throw new MoleculerError(err.message, err.code);
-                        }
-                        throw new MoleculerError(err, 500);
-                    });
-            }
-        },
-        list: {
-            auth: 'Basic',
-            cache: {
-              ttl: 60 * 60 // 1 hour
-            },
-            handler(): Promise<Membership[]> {
-                return this.adapter
-                    .find()
-                    .then((res: Membership[]) => {
-                        if (res.length !== 0) return res.map(membership => this.normalizeId(membership));
-                        throw new MoleculerError('No Membership found!', 404);
-                    })
-                    .catch((err: any) => {
-                        if (err.name === 'MoleculerError') {
-                            throw new MoleculerError(err.message, err.code);
-                        }
-                        throw new MoleculerError(err, 500);
-                    });
-            }
-        },
-        update: {}
+            throw new MoleculerError(err, 500);
+          });
+      }
     },
-    methods: {
-        /**
-         * Convert object _id to id
-         *
-         * @param {({_id: string})} obj
-         * @returns
-         */
-        normalizeId(obj: { _id: string;}) {
-            const newObj = {
-                id: obj._id,
-                ...obj
+    get: {
+      auth: 'Basic',
+      params: {
+        id: [{ type: 'string' }, { type: 'number' }]
+      },
+      cache: {
+        keys: ['id'],
+        ttl: 60 * 60 // 1 hour
+      },
+      handler(ctx: Context): Promise<Membership> {
+        return this.adapter
+          .findOne({ _id: ctx.params.id, active: true })
+          .then((res: Membership) => {
+            if (!res) {
+              return this.adapter
+                .findOne({ isDefault: true })
+                .then((def: Membership) => this.normalizeId(def));
             }
-            delete newObj._id;
-            return newObj;
-        },
+            return this.normalizeId(res);
+          })
+          .catch((err: any) => {
+            if (err.name === 'MoleculerError') {
+              throw new MoleculerError(err.message, err.code);
+            }
+            throw new MoleculerError(err, 500);
+          });
+      }
+    },
+    list: {
+      auth: 'Basic',
+      cache: {
+        ttl: 60 * 60 // 1 hour
+      },
+      handler(): Promise<Membership[]> {
+        return this.adapter
+          .find()
+          .then((res: Membership[]) => {
+            if (res.length !== 0) return res.map(membership => this.normalizeId(membership));
+            throw new MoleculerError('No Membership found!', 404);
+          })
+          .catch((err: any) => {
+            if (err.name === 'MoleculerError') {
+              throw new MoleculerError(err.message, err.code);
+            }
+            throw new MoleculerError(err, 500);
+          });
+      }
+    },
+    update: {
+      auth: 'Basic',
+      params: UpdateMembershipValidation,
+      async handler(ctx: Context): Promise<Membership> {
+        const { params } = ctx;
+        delete params.id;
+        return this.adapter
+          .updateById(ctx.params.id, { $set: { ...params } })
+          .then((res: Membership) => {
+            this.broker.cacher.clean(`membership.list:**`);
+            this.broker.cacher.clean(`membership.get:${ctx.params.id}**`);
+            return this.normalizeId(res);
+          })
+          .catch((err: any) => {
+            if (err.name === 'MoleculerError') {
+              throw new MoleculerError(err.message, err.code);
+            }
+            throw new MoleculerError(err, 500);
+          });
+      }
     }
+  },
+  methods: {
+    /**
+     * Convert object _id to id
+     *
+     * @param {({_id: string})} obj
+     * @returns
+     */
+    normalizeId(obj: { _id: string }) {
+      const newObj = {
+        id: obj._id,
+        ...obj
+      };
+      delete newObj._id;
+      return newObj;
+    }
+  }
 };
 
 export = TheService;
--- utilities/validations/membership.validate.ts
+++ utilities/validations/membership.validate.ts
@@ -1,40 +1,84 @@
 export const CreateMembershipValidation = {
-    id: { type: 'string', optional: true },
-    name: {
-        type: 'object',
-        props: {
-            tr: { type: 'string', optional: true },
-            en: { type: 'string' },
-            ar: { type: 'string', optional: true },
-            $$strict: true
-        }
+  id: { type: 'string', optional: true },
+  name: {
+    type: 'object',
+    props: {
+      tr: { type: 'string', optional: true },
+      en: { type: 'string' },
+      ar: { type: 'string', optional: true },
+      $$strict: true
+    }
+  },
+  tagline: {
+    type: 'object',
+    props: {
+      tr: { type: 'string', optional: true },
+      en: { type: 'string' },
+      ar: { type: 'string', optional: true },
+      $$strict: true
+    }
+  },
+  description: {
+    type: 'object',
+    props: {
+      tr: { type: 'string', optional: true },
+      en: { type: 'string' },
+      ar: { type: 'string', optional: true },
+      $$strict: true
+    }
+  },
+  sort: { type: 'number', integer: true, positive: true },
+  active: { type: 'boolean' },
+  public: { type: 'boolean' },
+  cost: { type: 'number', positive: true },
+  discount: { type: 'number', positive: true },
+  paymentFrequency: { type: 'number', integer: true, positive: true },
+  paymentFrequencyType: { type: 'enum', values: ['month', 'year'] },
+  attributes: { type: 'object' },
+  isDefault: { type: 'boolean', optional: true },
+  $$strict: true
+};
+
+export const UpdateMembershipValidation = {
+  id: { type: 'string', optional: true },
+  name: {
+    type: 'object',
+    props: {
+      tr: { type: 'string', optional: true },
+      en: { type: 'string', optional: true },
+      ar: { type: 'string', optional: true },
+      $$strict: true
     },
-    tagline: {
-        type: 'object',
-        props: {
-            tr: { type: 'string', optional: true },
-            en: { type: 'string' },
-            ar: { type: 'string', optional: true },
-            $$strict: true
-        }
-    },
-    description: {
-        type: 'object',
-        props: {
-            tr: { type: 'string', optional: true },
-            en: { type: 'string' },
-            ar: { type: 'string', optional: true },
-            $$strict: true
-        }
-    },
-    sort: { type: 'number', integer: true, positive: true },
-    active: { type: 'boolean',},
-	public: { type: 'boolean',},
-    cost: { type: 'number', positive: true },
-	discount: { type: 'number', positive: true },
-	paymentFrequency: { type: 'number', integer: true, positive: true },
-	paymentFrequencyType: { type: 'enum', values: ['month', 'year'] },
-    attributes: { type: 'object' },
-    isDefault: { type: 'boolean', optional: true },
-    $$strict: true
-}
+    optional: true
+  },
+  tagline: {
+    type: 'object',
+    props: {
+      tr: { type: 'string', optional: true },
+      en: { type: 'string', optional: true },
+      ar: { type: 'string', optional: true },
+      $$strict: true,
+      optional: true
+    }
+  },
+  description: {
+    type: 'object',
+    props: {
+      tr: { type: 'string', optional: true },
+      en: { type: 'string', optional: true },
+      ar: { type: 'string', optional: true },
+      $$strict: true,
+      optional: true
+    }
+  },
+  sort: { type: 'number', integer: true, positive: true, optional: true },
+  active: { type: 'boolean', optional: true },
+  public: { type: 'boolean', optional: true },
+  cost: { type: 'number', positive: true, optional: true },
+  discount: { type: 'number', positive: true, optional: true },
+  paymentFrequency: { type: 'number', integer: true, positive: true, optional: true },
+  paymentFrequencyType: { type: 'enum', values: ['month', 'year'], optional: true },
+  attributes: { type: 'object', optional: true },
+  isDefault: { type: 'boolean', optional: true },
+  $$strict: true
+};
