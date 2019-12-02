e7b27a85e442fab78f6a3fcbbcdc7a335773d422
Ahmed ElGreetly <elgreetly@knawat.com>
2019-12-02 19:01:40 +0300

    add: currency openapi

--- services/currencies.service.ts
+++ services/currencies.service.ts
@@ -1,5 +1,6 @@
 import { Context, ServiceSchema } from 'moleculer';
 import fetch from 'node-fetch';
+import { CurrenciesGetCurrenciesOpenapi, CurrenciesGetCurrencyOpenapi } from '../utilities/openapi';
 import { Currency } from './../types/currency.d';
 
 const TheService: ServiceSchema = {
@@ -12,55 +13,7 @@ const TheService: ServiceSchema = {
      * @returns {Currency}
      */
     getCurrency: {
-      openapi: {
-        $path: 'get /currencies/{currencyCode}',
-        summary: 'Get Currency By Code',
-        tags: ['Currencies', 'Enterprise Only'],
-        description: 'Gets currency code, name and rate',
-        parameters: [
-          {
-            name: 'currencyCode',
-            in: 'query',
-            required: false,
-            schema: {
-              type: 'string',
-              minLength: 3,
-              maxLength: 3
-            }
-          },
-          {
-            name: 'currencyCode',
-            in: 'path',
-            required: true,
-            schema: {
-              type: 'string'
-            }
-          }
-        ],
-        responses: {
-          '200': {
-            description: 'Status 200',
-            content: {
-              'application/json': {
-                schema: {
-                  $ref: '#/components/schemas/Currency'
-                }
-              }
-            }
-          },
-          '401': {
-            $ref: '#/components/responses/UnauthorizedErrorBasic'
-          },
-          '404': {
-            description: 'Status 404'
-          }
-        },
-        security: [
-          {
-            basicAuth: []
-          }
-        ]
-      },
+      openapi: CurrenciesGetCurrencyOpenapi,
       auth: 'Basic',
       cache: {
         keys: ['currencyCode'],
@@ -88,35 +41,7 @@ const TheService: ServiceSchema = {
      * @returns {Currency[]}
      */
     getCurrencies: {
-      openapi: {
-        $path: 'get /currencies',
-        summary: 'Get Currencies',
-        tags: ['Currencies', 'Enterprise Only'],
-        description: 'Get all currencies with names, code and rates info',
-        responses: {
-          '200': {
-            description: 'Status 200',
-            content: {
-              'application/json': {
-                schema: {
-                  type: 'array',
-                  items: {
-                    $ref: '#/components/schemas/Currency'
-                  }
-                }
-              }
-            }
-          },
-          '401': {
-            $ref: '#/components/responses/UnauthorizedErrorBasic'
-          }
-        },
-        security: [
-          {
-            basicAuth: []
-          }
-        ]
-      },
+      openapi: CurrenciesGetCurrenciesOpenapi,
       auth: 'Basic',
       cache: {
         ttl: 60 * 60 // 1 hour
---
+++ utilities/openapi/currencies.openapi.ts
@@ -0,0 +1,79 @@
+export const CurrenciesGetCurrencyOpenapi = {
+  $path: 'get /currencies/{currencyCode}',
+  summary: 'Get Currency By Code',
+  tags: ['Currencies', 'Enterprise Only'],
+  description: 'Gets currency code, name and rate',
+  parameters: [
+    {
+      name: 'currencyCode',
+      in: 'query',
+      required: false,
+      schema: {
+        type: 'string',
+        minLength: 3,
+        maxLength: 3
+      }
+    },
+    {
+      name: 'currencyCode',
+      in: 'path',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }
+  ],
+  responses: {
+    '200': {
+      description: 'Status 200',
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/components/schemas/Currency'
+          }
+        }
+      }
+    },
+    '401': {
+      $ref: '#/components/responses/UnauthorizedErrorBasic'
+    },
+    '404': {
+      description: 'Status 404'
+    }
+  },
+  security: [
+    {
+      basicAuth: [] as any[]
+    }
+  ]
+};
+
+export const CurrenciesGetCurrenciesOpenapi = {
+  $path: 'get /currencies',
+  summary: 'Get Currencies',
+  tags: ['Currencies', 'Enterprise Only'],
+  description: 'Get all currencies with names, code and rates info',
+  responses: {
+    '200': {
+      description: 'Status 200',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'array',
+            items: {
+              $ref: '#/components/schemas/Currency'
+            }
+          }
+        }
+      }
+    },
+    '401': {
+      $ref: '#/components/responses/UnauthorizedErrorBasic'
+    }
+  },
+  security: [
+    {
+      basicAuth: [] as any[]
+    }
+  ]
+};
