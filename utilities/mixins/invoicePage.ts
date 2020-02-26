79fef207cc5d1294926a90050d4bdc42a46e7115
Ahmed ElGreetly <elgreetly@knawat.com>
2020-02-26 16:07:42 +0300

    add: branded invoice title

--- utilities/mixins/invoicePage.ts
+++ utilities/mixins/invoicePage.ts
@@ -13,7 +13,7 @@ export const InvoicePage: ServiceSchema = {
             <head>
               <meta charset="UTF-8">
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
-              <title>Document</title>
+              <title>Knawat Invoice | ${order.orderNumber}</title>
             </head>
 
             <body>
