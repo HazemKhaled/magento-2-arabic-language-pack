2954d252cee76f93c43f036acb8acbf6000911a2
Hazem Khaled <hazem@knawat.com>
2020-06-18 18:42:23 +0300

    add autoRenew & renewed in docs

--- utilities/mixins/openapi/subscription.openapi.mixin.ts
+++ utilities/mixins/openapi/subscription.openapi.mixin.ts
@@ -221,6 +221,8 @@ const SubscriptionCreateOpenapi = {
             membership: { type: 'string', required: true },
             coupon: { type: 'string' },
             grantTo: { type: 'string', format: 'url', description: 'This field is used to donor the subscription to another store' },
+            autoRenew: { type: 'boolean' },
+            renewed: { type: 'boolean' },
           },
         },
       },
