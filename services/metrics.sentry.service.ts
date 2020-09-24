0d06f902af3f289c8e2a6dd6188bd2beb2e3dfdb
Hazem Khaled <hazem.khaled@gmail.com>
2020-09-24 21:59:21 +0300

    Add sentery

---
+++ services/metrics.sentry.service.ts
@@ -0,0 +1,18 @@
+import SentryMixin from 'moleculer-sentry';
+
+const TheService = {
+  mixins: [SentryMixin],
+  settings: {
+    /** @type {String} DSN given by sentry. */
+    dsn: process.env.SENTRY_DNS,
+    /** @type {Object?} Additional options for `Sentry.init` */
+    options: {},
+    /** @type {Object?} Options for the sentry scope */
+    scope: {
+      /** @type {String?} Name of the meta containing user infos */
+      user: 'user',
+    },
+  },
+};
+
+export = TheService;
