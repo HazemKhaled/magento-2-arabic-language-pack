411bc2de7e74738df7c481e96d5da5945a7d6263
Ahmed Elgreetly <aelgreetly@yahoo.com>
2020-09-25 18:26:34 +0300

    openapi lint

--- utilities/mixins/openapi/products-instances.openapi.mixin.ts
+++ utilities/mixins/openapi/products-instances.openapi.mixin.ts
@@ -374,7 +374,7 @@ const ProductsList = {
       name: 'lastupdate',
       in: 'query',
       description:
-        'Timestamp(seconds since Jan 01 1970. (UTC)) of last import run DateTime (must be in UTC), API will respond only products which are updated/created after this timestamp.',
+        'Timestamp(`seconds/milliseconds` since Jan 01 1970. (UTC)) of last import run DateTime (must be in UTC), API will respond only products which are updated/created after this timestamp.',
       example: '1542794072 for 21-11-2018 @ 9:54am',
       schema: {
         type: 'number',
