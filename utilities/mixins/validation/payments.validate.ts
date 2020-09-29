65367943f7510387d7acac113f0c75234f822915
Abdelrahman <abdelrahman@knawat.com>
2020-09-29 18:06:26 +0300

    feat: add validation

--- utilities/mixins/validation/payments.validate.ts
+++ utilities/mixins/validation/payments.validate.ts
@@ -1,5 +1,16 @@
 import { ServiceSchema } from 'moleculer';
 
+const checkoutValueProps = {
+  currency_code: {
+    type: 'string',
+    min: 3,
+  },
+  value: {
+    type: 'number',
+    convert: true,
+  },
+};
+
 export const PaymentsValidation: ServiceSchema = {
   name: 'payments',
   actions: {
@@ -80,6 +91,60 @@ export const PaymentsValidation: ServiceSchema = {
         payment_mode: {
           type: 'string',
           optional: true,
+        },
+      },
+    },
+    checkout: {
+      params: {
+        store: {
+          type: 'string',
+        },
+        hmac: {
+          type: 'string',
+        },
+        purchase_units: {
+          type: 'array',
+          items: {
+            type: 'object',
+            props: {
+              description: {
+                type: 'string',
+                optional: true,
+              },
+              type: {
+                type: 'enum',
+                optional: true,
+                values: ['order', 'subscription', 'charge'],
+              },
+              amount: {
+                type: 'object',
+                props: {
+                  ...checkoutValueProps,
+                  breakdown: {
+                    type: 'object',
+                    optional: true,
+                    props: {
+                      item_total: {
+                        type: 'object',
+                        optional: true,
+                        props: checkoutValueProps,
+                      },
+                      shipping: {
+                        type: 'object',
+                        optional: true,
+                        props: checkoutValueProps,
+                      },
+                      tax_total: {
+                        type: 'object',
+                        optional: true,
+                        props: checkoutValueProps,
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
         },
       },
     },
