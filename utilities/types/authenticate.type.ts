07cd40655bf26bbc67120ab256c4227d305c179f
Dhaval Dhrangadhariya <dhavalsoni9989@gmail.com>
2020-10-23 19:04:54 +0530

    fix: Resolved type issue

--- utilities/types/authenticate.type.ts
+++ utilities/types/authenticate.type.ts
@@ -24,6 +24,7 @@ export interface AuthorizeMeta {
  */
 export interface IncomingRequest extends IncomingMessage {
   $params: GenericObject;
+  $meta?: { storeId: string };
   $endpoint: {
     action: {
       auth: string;
