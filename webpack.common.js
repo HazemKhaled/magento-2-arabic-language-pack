c11179d1ac52c5b0a0240beaaf71547f5193c3db
Hazem Khaled <hazem.khaled@gmail.com>
2020-11-18 19:46:08 +0300

    remove babel

--- webpack.common.js
+++ webpack.common.js
@@ -34,11 +34,6 @@ module.exports = {
         },
       },
       {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
         test: /\.css$/,
         use: [
           // 'vue-style-loader',
