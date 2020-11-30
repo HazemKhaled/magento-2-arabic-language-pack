3f31b85e910a9ff8829edb1c65576b370776df9f
Rahul Jha <rahul.jha@tatvasoft.com>
2020-11-30 15:57:34 +0530

    Unit test: separate test script to execute unit test separately

---
+++ test/unit/unit.spec.ts
@@ -0,0 +1,5 @@
+/* The file will execute unit test cases in below mentioned order */
+
+require('../unit/services/products.spec.ts');
+require('../unit/services/orders.spec.ts');
+require('../unit/services/categories.spec.ts');
