560c4f2d592cb4c864faa602f9748f0019e9f349
Abdelrahman <abdelrahman@knawat.com>
2020-12-01 15:26:52 +0300

    debug: add logs

--- services/orders.service.ts
+++ services/orders.service.ts
@@ -837,12 +837,14 @@ const TheService: ServiceSchema = {
       ): Promise<GenericObject> {
         const { storeId, id } = ctx.params;
 
+        console.log('meta store', ctx.meta.store);
         const storeDoc: Partial<Store> =
           ctx.meta.store ||
           (await ctx.call<Store, Partial<Store>>('stores.sGet', {
             id: storeId,
           }));
 
+        console.log('meta doc', storeDoc);
         const order = await ctx.call<Order, Partial<OrderRequestParams>>(
           'orders.getOrder',
           { order_id: id }
