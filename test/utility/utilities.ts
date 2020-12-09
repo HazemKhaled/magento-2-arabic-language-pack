5defe429d643bf5c1c10196506bdfe46dcba5ba7
Hazem Khaled <hazem.khaled@gmail.com>
2020-12-09 16:37:39 +0300

    Remove optional chaining, not needed

--- test/utility/utilities.ts
+++ test/utility/utilities.ts
@@ -1,3 +1,11 @@
-export function arrayRandom<T>(arr: T[]): T {
-  return arr ? arr[Math.floor(Math.random() * arr?.length)] : null;
+/**
+ * get random item from an array
+ *
+ * @export
+ * @template T
+ * @param {T[]} [arr=[]]
+ * @returns {T}
+ */
+export function arrayRandom<T>(arr: T[] = []): T {
+  return arr ? arr[Math.floor(Math.random() * arr.length)] : null;
 }
