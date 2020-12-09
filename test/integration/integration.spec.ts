7f58576d04e7736543f7081ad2242a7f5bce4cd5
Hazem Khaled <hazem.khaled@gmail.com>
2020-12-09 11:50:34 +0300

    Cleanup, no need

--- test/integration/integration.spec.ts
+++
@@ -1,5 +0,0 @@
-/* This file is used to set order of execution of integration test files */
-
-import './categories';
-import './stores';
-import './token';
--- test/unit/unit.spec.ts
+++
@@ -1,4 +0,0 @@
-/* The file will execute unit test cases in below mentioned order */
-import './services/products.spec';
-import './services/orders.spec';
-import './services/categories.spec';
