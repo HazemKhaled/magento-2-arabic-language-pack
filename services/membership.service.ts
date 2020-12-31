56d53b41f83ec714e0c72b496cfbd7f68ee6e1bd
Dhaval Dhrangadhariya <dhavalsoni9989@gmail.com>
2020-12-31 18:03:11 +0530

    fix: Resolved the membership get issues

--- services/membership.service.ts
+++ services/membership.service.ts
@@ -82,20 +82,26 @@ const TheService: ServiceSchema = {
         }
 
         return ctx
-          .call<Membership[], GenericObject>('membership.find', {
-            query,
-          })
+          .call<Membership[], { query: Partial<MembershipRequestParams> }>(
+            'membership.find',
+            {
+              query,
+            }
+          )
           .then(async ([res]) => {
             if (!res) {
               return ctx
-                .call<Membership[], GenericObject>('membership.find', {
+                .call<
+                  Membership[],
+                  { query: Partial<MembershipRequestParams> }
+                >('membership.find', {
                   query: { isDefault: true },
                 })
                 .then(([def]) => this.normalize(def));
             }
 
             if (ctx.params.coupon) {
-              await this.applyCouponDiscount(ctx.params.coupon, res);
+              await this.applyCouponDiscount(ctx, res);
             }
 
             return this.normalize(res, country);
@@ -244,15 +250,25 @@ const TheService: ServiceSchema = {
         return newObj;
       });
     },
-    async applyCouponDiscount(couponCode, membership): Promise<void> {
-      const coupon: Coupon = await this.call('coupon.getOne', {
-        id: couponCode,
-        membership: membership.id,
-        type: 'subscription',
-      }).then(null, (err: unknown) => err);
-      if (coupon instanceof Error) {
-        throw new MoleculerError(coupon.message, Number(coupon.code));
-      }
+    async applyCouponDiscount(
+      ctx: Context<{ coupon: string }>,
+      membership
+    ): Promise<void> {
+      const coupon = await ctx
+        .call<Coupon, { id: string; membership: string; type: string }>(
+          'coupons.getOne',
+          {
+            id: ctx.params.coupon,
+            membership: membership.id,
+            type: 'subscription',
+          }
+        )
+        .then(coupon => {
+          return coupon as Coupon;
+        })
+        .catch(err => {
+          throw new MoleculerError(err.message, Number(err.code));
+        });
       let discount = 0;
       if (coupon) {
         switch (coupon.discount.total.type) {
